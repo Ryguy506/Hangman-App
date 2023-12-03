b7a46c530e11042501903e75c249eed4e0becd21
Ryguy <ryan70297@gmail.com>
2023-12-02 23:57:27 -0400

    added guesses counter and win loss msg

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
   const [word, setWord] = useState('')
   const [correctLetters, setCorrectLetters] = useState([])
   const [hint, setHint] = useState('')
-  // const [playAgain, setPlayAgain] = useState(false)
   const [incorrectGuesses, setIncorrectGuesses] = useState(0)
   const [playAgain, setPlayAgain] = useState(false)
 
@@ -42,7 +41,6 @@ function App() {
 
 
   useEffect(() => {
-    setCorrectLetters([])
     for (let i = 0; i < word.length; i++) {
       correctLetters.push('_')
       setCorrectLetters([...correctLetters])
@@ -50,20 +48,6 @@ function App() {
 
   }, [word])
 
-
-
-  useEffect(() => {
-    if (correctLetters.join('') === word && correctLetters.length > 0) {
-      document.getElementById('letters').classList.add('text-success')
-      document.querySelectorAll('.letter').forEach(item => {
-        item.disabled = true
-      })
-      setPlayAgain(true)
-
-
-    }
-
-  }, [correctLetters])
 
 
   function checkLetter(e) {
@@ -92,6 +76,101 @@ function App() {
   }
 
 
+
+
+  useEffect(() => {
+    if (correctLetters.join('') === word && correctLetters.length > 0) {
+      document.getElementById('letters').classList.add('text-success')
+      document.querySelectorAll('.letter').forEach(item => {
+        item.disabled = true
+      })
+      setPlayAgain(true)
+    }
+    else {
+      const canvas = document.getElementById('hangmanCanvas');
+      const ctx = canvas.getContext('2d');
+      ctx.lineWidth = 5;
+      switch (incorrectGuesses) {
+  
+        case 1:
+          ctx.beginPath();
+          ctx.moveTo(50, 350);
+          ctx.lineTo(150, 350);
+          ctx.stroke();
+          break;
+        case 2:
+          ctx.beginPath();
+          ctx.moveTo(100, 350);
+          ctx.lineTo(100, 50);
+          ctx.stroke();
+          break;
+        case 3:
+          ctx.beginPath();
+          ctx.moveTo(100, 50);
+          ctx.lineTo(200, 50);
+          ctx.stroke();
+          break;
+        case 4:
+          ctx.beginPath();
+          ctx.moveTo(200, 50);
+          ctx.lineTo(200, 100);
+          ctx.stroke();
+          break;
+        case 5:
+          ctx.beginPath();
+          ctx.arc(200, 125, 25, 0, Math.PI * 2, true);
+          ctx.stroke();
+          break;
+        case 6:
+          ctx.beginPath();
+          ctx.moveTo(200, 150);
+          ctx.lineTo(200, 250);
+          ctx.stroke();
+          break;
+        case 7:
+          ctx.beginPath();
+          ctx.moveTo(200, 250);
+          ctx.lineTo(150, 300);
+          ctx.stroke();
+          break;
+        case 8:
+          ctx.beginPath();
+          ctx.moveTo(200, 250);
+          ctx.lineTo(250, 300);
+          ctx.stroke();
+          break;
+        case 9:
+          ctx.beginPath();
+          ctx.moveTo(200, 175);
+          ctx.lineTo(150, 225);
+          ctx.stroke();
+          break;
+        case 10:
+          ctx.beginPath();
+          ctx.moveTo(200, 175);
+          ctx.lineTo(250, 225);
+          ctx.stroke();
+
+          document.getElementById('letters').classList.add('text-danger')
+          document.querySelectorAll('.letter').forEach(item => {
+            item.disabled = true
+          })
+  
+          word.split('').forEach((item, i) => {
+  
+            correctLetters.splice(i, 1, item)
+            setCorrectLetters([...correctLetters])
+  
+          });
+          setPlayAgain(true)
+          break;
+      }
+  
+    }
+  }, [correctLetters , incorrectGuesses])
+
+
+
   async function getHint() {
     try {
       const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.toLowerCase()}`)
@@ -103,89 +182,6 @@ function App() {
     }
 
   }
-
-  useEffect(() => {
-    const canvas = document.getElementById('hangmanCanvas');
-    const ctx = canvas.getContext('2d');
-    ctx.lineWidth = 5;
-
-    switch (incorrectGuesses) {
-
-      case 1:
-        ctx.beginPath();
-        ctx.moveTo(50, 350);
-        ctx.lineTo(150, 350);
-        ctx.stroke();
-        break;
-      case 2:
-        ctx.beginPath();
-        ctx.moveTo(100, 350);
-        ctx.lineTo(100, 50);
-        ctx.stroke();
-        break;
-      case 3:
-        ctx.beginPath();
-        ctx.moveTo(100, 50);
-        ctx.lineTo(200, 50);
-        ctx.stroke();
-        break;
-      case 4:
-        ctx.beginPath();
-        ctx.moveTo(200, 50);
-        ctx.lineTo(200, 100);
-        ctx.stroke();
-        break;
-      case 5:
-        ctx.beginPath();
-        ctx.arc(200, 125, 25, 0, Math.PI * 2, true);
-        ctx.stroke();
-        break;
-      case 6:
-        ctx.beginPath();
-        ctx.moveTo(200, 150);
-        ctx.lineTo(200, 250);
-        ctx.stroke();
-        break;
-      case 7:
-        ctx.beginPath();
-        ctx.moveTo(200, 250);
-        ctx.lineTo(150, 300);
-        ctx.stroke();
-        break;
-      case 8:
-        ctx.beginPath();
-        ctx.moveTo(200, 250);
-        ctx.lineTo(250, 300);
-        ctx.stroke();
-        break;
-      case 9:
-        ctx.beginPath();
-        ctx.moveTo(200, 175);
-        ctx.lineTo(150, 225);
-        ctx.stroke();
-        break;
-      case 10:
-        ctx.beginPath();
-        ctx.moveTo(200, 175);
-        ctx.lineTo(250, 225);
-        ctx.stroke();
-        document.getElementById('letters').classList.add('text-danger')
-        document.querySelectorAll('.letter').forEach(item => {
-          item.disabled = true
-        })
-
-        word.split('').forEach((item, i) => {
-
-          correctLetters.splice(i, 1, item)
-          setCorrectLetters([...correctLetters])
-
-        });
-        setPlayAgain(true)
-        break;
-    }
-
-
-  }, [incorrectGuesses])
 
 
 
@@ -201,7 +197,10 @@ function App() {
 
 
         <div className='row'>
-          <div className='col-5 border'>
+          <div className='col-5 border text-center'>
+            {incorrectGuesses === 10 ? <h2 className='text-danger'>Game Over</h2> : 
+            incorrectGuesses < 10 && correctLetters.join('') === word && correctLetters.length > 0 
+            ? <h2 className='text-success'> You Win</h2> : <h2>{10 - incorrectGuesses} Guesses Remaining </h2>}
 
             <canvas id="hangmanCanvas" width="400" height="400"></canvas>
           </div>
